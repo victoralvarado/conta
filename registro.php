909eb1fe352921ef03a7a9963039338c63cacfec
DiegoRosa98 <diego.rosa316@gmail.com>
2021-03-28 11:51:35 -0600

    Registro de usuarios.

---
+++ registro.php
@@ -0,0 +1,47 @@
+
+    <!DOCTYPE html>
+<html lang="en" class="app">
+<head>
+  <meta charset="utf-8" />
+  <title>Registro</title>
+  <meta name="description" content="app, web app, responsive, admin dashboard, admin, flat, flat ui, ui kit, off screen nav" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
+  <?php include("referencias.php"); ?> 
+</head>
+<body>
+  <section class="vbox">
+    <header class="bg-dark dk header navbar navbar-fixed-top-xs">
+      <div class="navbar-header aside-md">
+        <a class="btn btn-link visible-xs" data-toggle="class:nav-off-screen,open" data-target="#nav,html">
+          <i class="fa fa-bars"></i>
+        </a>
+        <a href="#" class="navbar-brand" data-toggle="fullscreen"><img src="images/logo.png" class="m-r-sm">&nbsp;Registro</a>
+        <a class="btn btn-link visible-xs" data-toggle="dropdown" data-target=".nav-user">
+          <i class="fa fa-cog"></i>
+        </a>
+      </div>
+                 
+    </header>
+
+    <section>
+      <section class="hbox stretch">
+
+       <link rel="stylesheet" type="text/css" href="css/css.css">
+
+
+        <section id="content">
+          <section class="vbox">        
+              
+          </section>
+          <a href="#" class="hide nav-off-screen-block" data-toggle="class:nav-off-screen" data-target="#nav"></a>
+        </section>
+        <aside class="bg-light lter b-l aside-md hide" id="notes">
+          <div class="wrapper">Notification</div>
+        </aside>
+      </section>
+    </section>
+  </section>
+  
+
+</body>
+</html>
