d67a0e6cc19fda881ffb10eb929d37bf7fdde07e
VA <alvaradoguzman900@gmail.com>
2021-03-31 17:27:41 -0600

    Update productocontroller.php

--- controller/productocontroller.php
+++ controller/productocontroller.php
@@ -5,23 +5,31 @@ if (isset($_POST['agregarProducto'])) {
     insertProducto();
 }
 
-if (isset($_POST['key'])) {
-    $key = $_POST['key'];
-    if ($key == 'elimProducto') {
-        eraseProducto();
-    }
+if (isset($_POST['editarProducto'])) {
+    editProducto();
 }
 
+if (isset($_POST['idD'])) {
+    $id = $_POST["idD"];
+    eraseProducto($id);
+}
 
 function insertProducto()
 {
     $objP = new Producto();
+    $nombreDoc = $_FILES['imagen']['name'];
+	$archivoDoc = $_FILES['imagen']['tmp_name'];
+    $rutaDocServer = "../img";
+    $rutaDocDB = 'img';
+	$rutaDocServer=$rutaDocServer."/".$nombreDoc;
+	$rutaDocDB=$rutaDocDB."/".$nombreDoc;
+    move_uploaded_file($archivoDoc, $rutaDocServer);
     $objP->setNombre($_POST['nombre']);
     $objP->setExistencias($_POST['existencias']);
     $objP->setPrecio($_POST['precio']);
     $objP->setCosto($_POST['costo']);
     $objP->setDescripcion($_POST['descripcion']);
-    $objP->setImagen(addslashes(file_get_contents($_FILES['imagen']['tmp_name'])));
+    $objP->setImagen($rutaDocDB);
     $objP->setCodigo($_POST['codigo']);
     $res = $objP->saveProducto();
     if ($res['estado']) {
@@ -35,4 +43,73 @@ function insertProducto()
 				location.assign("http://localhost/conta/producto.php");
 		</script>';
     }
+}
+
+function editProducto()
+{
+    $objP = new Producto();
+    if ($_FILES['imagen']['name']==null) {
+        $objP->setNombre($_POST['nombre']);
+        $objP->setExistencias($_POST['existencias']);
+        $objP->setPrecio($_POST['precio']);
+        $objP->setCosto($_POST['costo']);
+        $objP->setDescripcion($_POST['descripcion']);
+        $objP->setImagen('');
+        $objP->setCodigo($_POST['codigo']);
+        $objP->setId($_POST['id']);
+        $res = $objP->updateProducto();
+        if ($res['estado']) {
+            echo '
+            <script type="text/javascript">
+                    location.assign("http://localhost/conta/producto.php");
+            </script>';
+        } else {
+            echo '
+            <script type="text/javascript">
+                    location.assign("http://localhost/conta/producto.php");
+            </script>';
+        }
+    } else{
+        unlink("../".$_POST['img']);
+        $nombreDoc = $_FILES['imagen']['name'];
+        $archivoDoc = $_FILES['imagen']['tmp_name'];
+        $rutaDocServer = "../img";
+        $rutaDocDB = 'img';
+        $rutaDocServer = $rutaDocServer . "/" . $nombreDoc;
+        $rutaDocDB = $rutaDocDB . "/" . $nombreDoc;
+        move_uploaded_file($archivoDoc, $rutaDocServer);
+        $objP->setNombre($_POST['nombre']);
+        $objP->setExistencias($_POST['existencias']);
+        $objP->setPrecio($_POST['precio']);
+        $objP->setCosto($_POST['costo']);
+        $objP->setDescripcion($_POST['descripcion']);
+        $objP->setImagen($rutaDocDB);
+        $objP->setCodigo($_POST['codigo']);
+        $objP->setId($_POST['id']);
+        $res = $objP->updateProducto();
+        if ($res['estado']) {
+            echo '
+            <script type="text/javascript">
+                    location.assign("http://localhost/conta/producto.php");
+            </script>';
+        } else {
+            echo '
+            <script type="text/javascript">
+                    location.assign("http://localhost/conta/producto.php");
+            </script>';
+        }
+    }
+    
+}
+
+function eraseProducto($id)
+{
+	$objP = new Producto();
+	$objP->setId($id);
+	$objP->deleteProducto();
+    echo '
+		<script type="text/javascript">
+				location.assign("http://localhost/conta/producto.php");
+		</script>';
+	//echo json_encode($res);
 }
