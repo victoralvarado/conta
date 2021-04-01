a53c59e8df8e2aef6799b768f4a506c583178f07
VA <alvaradoguzman900@gmail.com>
2021-04-01 00:33:46 -0600

    Update updatePr.php

--- updatePr.php
+++ updatePr.php
@@ -9,48 +9,48 @@
             </div>
             <div class="modal-body">
                 <?php ?>
-                <form method="POST" action="controller/productocontroller.php" enctype="multipart/form-data">
+                <form method="POST" action="controller/productoController.php" enctype="multipart/form-data">
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Nombre</label>
+                        <label class="col-md-4 control-label">Nombre:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><input id="nombre" name="nombre" placeholder="Nombre del producto" class="form-control" required="true" value="<?php echo $value['nombre']; ?>" type="text"></div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Existencias</label>
+                        <label class="col-md-4 control-label">Existencias:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><input id="existencias" name="existencias" placeholder="Existencias" min="1" class="form-control" required="true" value="<?php echo $value['existencias']; ?>" type="number"></div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Precio</label>
+                        <label class="col-md-4 control-label">Precio:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><input id="precio" name="precio" placeholder="Precio" min="1.00" step="any" class="form-control" required="true" value="<?php echo $value['precio']; ?>" type="number"></div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Costo</label>
+                        <label class="col-md-4 control-label">Costo:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><input id="costo" name="costo" placeholder="Costo" min="1.00" step="any" class="form-control" required="true" value="<?php echo $value['costo']; ?>" type="number"></div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Descripción</label>
+                        <label class="col-md-4 control-label">Descripción:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><textarea id="descripcion" name="descripcion" placeholder="Descripción" class="form-control" rows="2"><?php echo $value['descripcion']; ?></textarea></div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Imagen</label>
+                        <label class="col-md-4 control-label">Imagen:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><input id="imagen" name="imagen" class="form-control" accept="image/*" type="file">
-                            <div class="alert alert-info" role="alert">
+                            <div class="alert alert-warning" role="alert">
                             Si no agrega una imagen nueva, se mantendra la imagen actual</div>
                             </div>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="col-md-4 control-label">Codigo</label>
+                        <label class="col-md-4 control-label">Codigo:</label>
                         <div class="col-md-8 inputGroupContainer">
                             <div class="input-group"><input id="codigo" name="codigo" placeholder="Codigo del producto" class="form-control" required="true" value="<?php echo $value['codigo']; ?>" type="text"></div>
                         </div>
