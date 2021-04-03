f28fc0b7d4569fb05c3bd1b0d440244831870087
VA <alvaradoguzman900@gmail.com>
2021-04-02 23:51:02 -0600

    Update producto.php

--- producto.php
+++ producto.php
@@ -107,11 +107,11 @@
                                             foreach ($data as $value) {
                                         ?>
                                                 <tr>
-                                                    <td><?php echo $value['nombre']; ?></td>
+                                                    <td><?php echo ucwords(strtolower($value['nombre'])); ?></td>
                                                     <td><?php echo $value['existencias']; ?></td>
                                                     <td><?php echo $value['precio']; ?></td>
                                                     <td><?php echo $value['costo']; ?></td>
-                                                    <td><?php echo $value['descripcion']; ?></td>
+                                                    <td><?php echo ucwords(strtolower($value['descripcion'])); ?></td>
                                                     <td><?php echo $value['codigo']; ?></td>
                                                     <td><img src="<?php echo $value['imagen']; ?>" width="150" alt="<?php echo $value['nombre']; ?>" /></td>
                                                     <td>
@@ -124,68 +124,68 @@
                                                         </form>
                                                     </td>
                                                 </tr>
-                                                <div class="modal fade" id="edit_<?php echo $value['id']; ?>" tabindex="-1" role="dialog" aria-hidden="true">
-                                                    <div class="modal-dialog" role="document">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title">Editar Producto</h5>
-                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
+                                    </tbody>
+                                    <div class="modal fade" id="edit_<?php echo $value['id']; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h6 class="modal-title"><strong>Editar Producto</strong></h6>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form method="POST" action="controller/productoController.php" enctype="multipart/form-data">
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Nombre:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <input id="nombreEdit" name="nombre" placeholder="Nombre del producto" class="form-control" required="true" value="<?php echo $value['nombre']; ?>" type="text">
                                                             </div>
-                                                            <div class="modal-body">
-                                                                <?php ?>
-                                                                <form method="POST" action="controller/productoController.php" enctype="multipart/form-data">
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Nombre:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><input id="nombre" name="nombre" placeholder="Nombre del producto" class="form-control" required="true" value="<?php echo $value['nombre']; ?>" type="text"></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Existencias:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><input id="existencias" name="existencias" placeholder="Existencias" min="1" class="form-control" required="true" value="<?php echo $value['existencias']; ?>" type="number"></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Precio:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><input id="precio" name="precio" placeholder="Precio" min="1.00" step="any" class="form-control" required="true" value="<?php echo $value['precio']; ?>" type="number"></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Costo:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><input id="costo" name="costo" placeholder="Costo" min="1.00" step="any" class="form-control" required="true" value="<?php echo $value['costo']; ?>" type="number"></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Descripción:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><textarea id="descripcion" name="descripcion" placeholder="Descripción" class="form-control" rows="2"><?php echo $value['descripcion']; ?></textarea></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Imagen:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><input id="imagen" name="imagen" class="form-control" accept="image/*" type="file">
-                                                                                <div class="alert alert-warning" role="alert">
-                                                                                    Si no agrega una imagen nueva, se mantendra la imagen actual</div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label class="col-md-4 control-label">Codigo:</label>
-                                                                        <div class="col-md-8 inputGroupContainer">
-                                                                            <div class="input-group"><input id="codigo" name="codigo" placeholder="Codigo del producto" class="form-control" required="true" value="<?php echo $value['codigo']; ?>" type="text"></div>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="modal-footer">
-                                                                        <input id="img" name="img" class="form-control" required="true" value="<?php echo $value['imagen']; ?>" type="hidden">
-                                                                        <input id="id" name="id" min="1" class="form-control" required="true" value="<?php echo $value['id']; ?>" type="hidden">
-                                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                                        <button type="submit" id="editarProducto" name="editarProducto" class="btn btn-primary"">Editar</button>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Existencias:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <input id="existenciasEdit" name="existencias" placeholder="Existencias" min="1" class="form-control" required="true" value="<?php echo $value['existencias']; ?>" type="number">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Precio:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <input id="precioEdit" name="precio" placeholder="Precio" min="1.00" step="any" class="form-control" required="true" value="<?php echo $value['precio']; ?>" type="number">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Costo:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <input id="costoEdit" name="costo" placeholder="Costo" min="1.00" step="any" class="form-control" required="true" value="<?php echo $value['costo']; ?>" type="number">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Descripción:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <textarea id="descripcionEdit" name="descripcion" placeholder="Descripción" class="form-control" rows="2"><?php echo $value['descripcion']; ?></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Imagen:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <input id="imagenEdit" name="imagen" class="form-control" accept="image/*" type="file">
+                                                                <div class="alert alert-warning" role="alert">
+                                                                    Si no agrega una imagen nueva, se mantendra la imagen actual
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group row">
+                                                            <label class="col-md-4 control-label">Codigo:</label>
+                                                            <div class="col-md-8 inputGroupContainer">
+                                                                <input id="codigoEdit" name="codigo" placeholder="Codigo del producto" class="form-control" required="true" value="<?php echo $value['codigo']; ?>" type="text">
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <input id="img" name="img" class="form-control" required="true" value="<?php echo $value['imagen']; ?>" type="hidden">
+                                                            <input id="id" name="id" min="1" class="form-control" required="true" value="<?php echo $value['id']; ?>" type="hidden">
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                            <button type="submit" id="editarProducto" name="editarProducto" class="btn btn-primary"">Editar</button>
                                                                     </div>
                                                                 </form>
                                                             </div>
@@ -196,12 +196,11 @@
 
                                             }
                                         }
-                                        ?>
-                                    </tbody>
+                                        ?>      
                                 </table>
+                            </div>
                         </section>
-                        <aside class=" bg-light lter b-l aside-md hide" id="notes">
-                                                                            </aside>
+                        <aside class=" bg-light lter b-l aside-md hide" id="notes"></aside>
                         </section>
                 </div>
             </section>
