7b57062d35d3cef9646f19f0531750ab2a0b4d4f
VA <alvaradoguzman900@gmail.com>
2021-04-03 01:34:50 -0600

    Update proveedorController.php

--- controller/proveedorController.php
+++ controller/proveedorController.php
@@ -2,7 +2,7 @@
 require_once('../model/Proveedor.php');
 
 if (isset($_POST['agregarProveedor'])) {
-    insertProducto();
+    insertProveedor();
 }
 
 if (isset($_POST['editarProveedor'])) {
@@ -12,4 +12,29 @@ if (isset($_POST['editarProveedor'])) {
 if (isset($_POST['idD'])) {
     $id = $_POST["idD"];
     eraseProducto($id);
+}
+
+function insertProveedor()
+{
+    $objP = new Proveedor();
+    $objP->setTipo($_POST['tipo']);
+    $objP->setClasificacion($_POST['clasificacion']);
+    $objP->setNit(str_replace("-","",$_POST['nit']));
+    $objP->setNrc(str_replace("-","",$_POST['nrc']));
+    $objP->setNombre($_POST['nombre']);
+    $objP->setRazon_social($_POST['razonsocial']);
+    $objP->setDireccion($_POST['direccion']);
+    $objP->setTelefono($_POST['telefono']);
+    $res = $objP->saveProveedor();
+    if ($res['estado']) {
+        echo '
+		<script type="text/javascript">
+				location.assign("../proveedor.php");
+		</script>';
+    } else {
+        echo '
+		<script type="text/javascript">
+				location.assign("../proveedor.php");
+		</script>';
+    }
 }
