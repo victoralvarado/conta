05090715344882918b53305d65c8251f15506b1c
VA <alvaradoguzman900@gmail.com>
2021-04-03 12:18:44 -0600

    Update proveedor.php

--- proveedor.php
+++ proveedor.php
@@ -99,7 +99,7 @@
                                                     <div class="form-group">
                                                         <center>
                                                             <button type="submit" id="agregarProveedor" name="agregarProveedor" class="btn btn-primary"><em class="fa fa-plus"></em> Agregar</button>
-                                                            <button type="reset" class="btn btn-warning"><em class="fa fa-eraser"></em> Cancelar</button>
+                                                            <button type="reset" class="btn btn-warning" data-toggle="tooltip" data-placement="top" title="Limpiar cajas de texto"><em class="fa fa-eraser"></em> Limpiar</button>
                                                         </center>
                                                     </div>
                                                 </fieldset>
@@ -160,7 +160,7 @@
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <h6 class="modal-title"><strong>Editar Proveedor</strong></h6>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <button type="button" class="close" data-dismiss="modal" onclick="location.reload()" aria-label="Close">
                                                         <span aria-hidden="true">&times;</span>
                                                     </button>
                                                 </div>
@@ -283,7 +283,7 @@
                                                         <div class="form-group row">
                                                             <label class="col-md-4 control-label">Dirección:</label>
                                                             <div class="col-md-8 inputGroupContainer">
-                                                                <textarea name="direccionEdit" id="direccion" placeholder="Dirección" class="form-control" rows="2"><?php echo $value['direccion']; ?></textarea>
+                                                                <textarea id="direccionEdit" name="direccion" placeholder="Dirección" class="form-control" rows="2"><?php echo $value['direccion']; ?></textarea>
                                                             </div>
                                                         </div>
                                                         <div class="form-group row">
@@ -294,8 +294,8 @@
                                                         </div>
                                                         <div class="modal-footer">
                                                             <input id="id" name="id" min="1" class="form-control" required="true" value="<?php echo $value['id']; ?>" type="hidden">
-                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                            <button type="submit" id="editarProveedor" name="editarProveedor" class="btn btn-primary"">Editar</button>
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="location.reload()"><i class="fa fa-times"></i> Cancelar</button>
+                                                            <button type="submit" id="editarProveedor" name="editarProveedor" class="btn btn-primary""><i class="fa fa-pencil-square-o"></i> Editar</button>
                                                                     </div>
                                                                 </form>
                                                             </div>
