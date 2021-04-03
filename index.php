0f62a3aa4f86d39eed1dc4061ed75c5e672d5c86
VA <alvaradoguzman900@gmail.com>
2021-04-03 00:22:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <section>
       <section class="hbox stretch">
 
-        <?php include("nav.php"); ?>
+        <?php $iva = "active"; include("nav.php"); ?>
 
         <div class="ex1">
           <section id="content" class="container-fluid">
